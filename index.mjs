79dd62cf2cbb2fdf74c99e5f00d9709d9cbc8a2e
sagar460 <shresthasagr7000@gmail.com>
2024-06-18 19:19:57 +0545

    first commit

---
+++ index.mjs
@@ -0,0 +1,81 @@
+// console.log("an")
+// filter
+// ()=>{}
+// filter only returns true or false
+// let list = ["a","b","c"]
+// let list1 = list.filter((value, index)=> {
+//     return true;
+// });
+// console.log(list1)
+// let list = ["a","b","c","d","e"];
+// let output = list.filter((value, index)=>{
+//     if(value === "e"){
+//     return true;
+// }
+// else{
+//     return false;
+//     console.log(output)
+// };
+
+// let input = [1,2,3,6]
+// let output = input.filter((value,index)=>{
+//     if(value % 2 === 0){
+//         return true;
+//     }
+// else{
+//     return false;
+// }});
+// console.log(output)
+
+// let list = [1,2,3,4];
+// let list1 = list.find((value, index)=>{
+// return true;
+// })
+// console.log(list1);
+
+// let list = ["ram", "",1,0, null, undefined, true, false];
+// let output = list.filter(Boolean);
+// console.log(output);
+
+// let list = ["ram", "hari", "shyam", "roshan"]
+// let output = list.filter((value, index)=>{
+//     if (value.length > 4)
+//     return true;
+// else {
+//     return false;
+// }
+// })
+// console.log(output)
+
+// for each
+// let list = [1,2,3]
+// let output = list.forEach((value, index)=>{
+//     console.log(value)
+// })
+
+// includes
+// let list = [ "ram", "shyam", "Hari",]
+// let output = list.includes("ram");
+// console.log(output);
+
+// let list = [ 1,2,3,4];
+// list.push(5);
+// list.pop();
+// list.unshift(5);
+// list.shift();
+// console.log(list)
+
+// push => push value to the last
+// pop => remove value from last
+// unshift => push value to first
+// shift => remove value to first
+
+// sort
+
+let list = ["d", "r", "t", "k","z", "a", "A"]
+let output = list.sort();
+console.log(output)
+
+// let list = ["anish", "Manish", "jeevan", "rohit", "milan", "milaz", "Z"]
+// let output = list.sort()
+// console.log(output)
