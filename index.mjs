a664e7ec402424d003d62dc3324ec8b40c102f8d
sagar460 <shresthasagr7000@gmail.com>
2024-06-21 11:17:16 +0545

    second commit

--- index.mjs
+++ index.mjs
@@ -72,10 +72,134 @@
 
 // sort
 
-let list = ["d", "r", "t", "k","z", "a", "A"]
-let output = list.sort();
-console.log(output)
+// let list = ["d", "r", "t", "k","z", "a", "A"]
+// let output = list.sort();
+// console.log(output)
 
 // let list = ["anish", "Manish", "jeevan", "rohit", "milan", "milaz", "Z"]
 // let output = list.sort()
 // console.log(output)
+
+
+// slice
+// let list =[1,2,3,4];
+// let newlist = list.slice(3)
+// console.log(newlist);
+
+
+// reduce
+
+ 
+// let list = [1,2,3,4,5,8,10];
+// let newlist = list.filter((value,index)=>{
+//         if(value % 2 === 0){
+//             return true;
+//         }
+//     else{
+//         return false;
+//     }});
+   
+//     let sum = newlist.reduce((previous,current)=>{
+//         return previous + current ;
+
+//     },0)
+//     console.log(sum)
+
+// object = set of key and values
+// array is a collection of values whereas object is a collection of key value pairs. key value pairs are called properties.
+// let info = {
+//     name: "ram",
+//     age: 23,
+//     married: false,
+//     location: "chabel",
+// }
+// console.log(info.age)
+// console.log(info.name)
+// console.log(info)
+
+// change value
+// info.name= "hari"
+// info.age= 50
+// console.log(info)
+
+
+// delete
+// delete info.married
+// console.log(info)
+
+// let data ={
+//     name:"name",
+//     age:"22",
+//     name:"same",
+// }
+// console.log(data.name)
+// let data = {
+//     name: "ram",
+//     favfruits: ["apple", "banana,"],
+// location:{
+//     country:"nepal",
+//     province: "bagmati",
+//     City: "Ktm",
+//     street : 2728,
+// },
+// age: function(){
+//     console.log("my name is ram and i am from bagmati");
+
+// },
+// }
+//  console.log(data.favfruits[0])
+//  console.log(data.location.street)
+//  data.age();
+
+// alias
+// Alias means changing variable names. Once you change variable names you can 
+
+
+// let {
+//     name: fullname,
+//     age:year,
+//     ismarried,
+// }={
+//     name:"ram",
+//     age:"34",
+//     ismarried:false,
+
+// }
+// console.log(fullname)
+
+
+
+
+// let info = {
+//     name: "tam",
+//     surname: "paudel",
+//     fullname: function (){
+//         console.log(this.name)
+//         console.log(this.surname)
+//     },
+// };
+// console.log(info.name);
+// info.fullname();
+
+
+// array destructure
+
+// let planet = ["moon", "sun"];
+// // let a = planet[0]
+// // let b = planet[1]
+
+
+// let [a,b,c="venus",]= planet;
+// console.log(a,b,c)
+
+
+// spread Operator => ...
+// let planet = [ "sun", "moon", "pluto"]
+// let [planet1, ...rest]= planet;
+// console.log(planet1)  
+// console.log(rest);
+
+// let name = [ "anish", "ram", "hari", "shyam"]
+// let data = [ "school", "kathmandu", "nepal"]
+// let totaldata= ["this is my data", ...data];
+// console.log(totaldata)
---
+++ task.mjs
@@ -0,0 +1,87 @@
+
+// get the total  value of array of object price
+
+// let list = [ 
+//     {name: "mobile", price: 10000 },
+//     {name: "battery", price: 8000},
+//     {name: "earphone", price:5000},
+//     {name:"charger", price: 2000},]
+//     let newlist = list.map((value,index) => {
+//             return value.price;
+//           });
+//           console.log(newlist)
+//           let output = newlist.sort();
+//           console.log(output);
+
+
+//    let newlist = list.sort((a,b)=>{    //sort
+//         return a.price - b.price;
+//     })
+//     console.log(newlist);
+//    let newlist = list.map((value,index) => {
+//     return value.price;
+//    });
+//    console.log(newlist)
+//    let sum = newlist.reduce((a,b) => {
+// return a+b;
+//    },0)
+//    console.log(sum);
+
+
+// sort the array of object according to price
+
+// let sortlist = newlist.sort((a,b)=>{
+// return a-b;
+// })
+// console.log(sortlist)
+
+// make an arrray function
+
+// if age is 25, console your ticket is free
+// if age is 26, console you ticket is 100
+// if age is other than 25,26  - console you are not eligible
+
+// let fun = (age)=>{
+//     if( age === 25){
+//         console.log("your ticket is free")
+//     }
+//     else if ( age === 26){
+//         console.log("your ticket is 100 ")
+//     }
+//     else{
+//        console.log("you are not eligible")}
+// }
+// let output = fun(26)
+
+
+
+
+
+// make an array function named sum = num1,num2
+// pass 2 input and it must return the sum of num1 and num2 is num3
+// here num1, num2, num3 is number and do this by using string lateral
+
+// let fun = (num1, num2)=>{
+//     let num3 = `the sum of ${num1} and ${num2} is ${num1+num2}`
+// console.log(num3);
+// }
+// fun(1,2)
+
+
+// let list = [ "ram", "shyam", "Hari",]
+// let output = list.includes("ram");
+// console.log(output);
+
+// make an array function
+// pass away of fruits
+// the funtion must return "the fruits contain apple" if the array  contain apple. ELse return " the fruits does not contain apple"
+
+// let fun = ((fruits)=>{
+// if(fruits.includes("apple")){
+//     console.log("the fruit contains apple")
+// }
+// else{
+//     console.log("fruit does not contain apple")
+// }
+// })
+// fun(["orange","banana"])
